01d9d22815164765725598236d39f74cc33afe3f
Cameron Barker <cameronbarker@miradoranalytics.com>
2022-09-28 14:19:27 +0100

    New file for testing

---
+++ tests/test_calculator.py
@@ -0,0 +1,25 @@
+from calculator import *
+import pytest
+
+
+def test_add():
+    assert add(0, 1) == 1
+
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+
+
+def test_multiply():
+    assert multiply(6, 3) == 18
+
+
+def test_divide():
+    assert divide(8, 4) == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(10, 0)
+
+
